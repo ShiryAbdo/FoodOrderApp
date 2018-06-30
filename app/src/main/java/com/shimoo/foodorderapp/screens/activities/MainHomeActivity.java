6c0b389d24c4c976b4c7f6b42fa1c7a1b76d68c9
shimoo <shaimaaenfikry@gmail.com>
2018-06-30 00:27:58 -0700

    up date

--- app/src/main/java/com/shimoo/foodorderapp/screens/activities/MainHomeActivity.java
+++ app/src/main/java/com/shimoo/foodorderapp/screens/activities/MainHomeActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.shimoo.foodorderapp.R;
+
 public class MainHomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
